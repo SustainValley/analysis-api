eb9326dfc6704e6fff2d305c7d3a77ba18843343
yujining3827 <j_yujin_swu22@naver.com>
2025-08-22 03:34:48 +0900

    fix : DB 불러오는 코드 수정

--- app/db.py
+++ app/db.py
@@ -1,11 +1,21 @@
 # app/db.py
 # DB 연결
 
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base, Session
 
-# DB 연결 URL 형식: mysql+pymysql://username:password@host:port/database
-DB_URL = "mysql+pymysql://root:3827@localhost:3306/hackathon_db"
+
+load_dotenv()  # .env 파일 로드
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 # SQLAlchemy 엔진 생성
 engine = create_engine(DB_URL, echo=True, future=True)
